537223635f74bd18b27648495f2ba2b79a83fa9c
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-11-09 15:04:37 +0100

    added event to button

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btnPlayEl= document.querySelector("button");
+const gridContainerEl = document.querySelector(".container");
+
+
+btnPlayEl.addEventListener("click", function(){
+
+    generateCells(gridContainerEl);
+
+})
